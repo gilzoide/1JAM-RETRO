f8a18d4a837b8064aaaa61a56acddbbae12114fb
gilzoide <gilzoide@gmail.com>
2020-05-07 10:21:43 -0300

    Change manual Radar lighting by spotlight at Player scene

--- Control/MovementControl.gd
+++ Control/MovementControl.gd
@@ -3,7 +3,14 @@ extends KinematicBody
 export(float) var linear_speed = 5
 export(float) var angular_speed = deg2rad(60)
 var is_moving = false
+
 onready var animationPlayer = $AnimationPlayer
+onready var spotlight: SpotLight = $SpotLight
+export(Array, float) var spotlight_ranges = [8, 6, 4, 0]
+var spotlight_current_range = 0
+
+func _ready():
+	refresh_light()
 
 func _process(delta):
 	var rotation = 0
@@ -15,6 +22,9 @@ func _process(delta):
 	
 	if is_moving and not animationPlayer.is_playing():
 		animationPlayer.play("PlayerLinearMove")
+	
+	if Input.is_action_just_pressed("toggle_lights"):
+		toggle_lights()
 
 func _physics_process(_delta):
 	var movement = Vector3()
@@ -34,3 +44,10 @@ func _physics_process(_delta):
 func stop_movement_animation():
 	if not is_moving:
 		animationPlayer.stop()
+
+func toggle_lights():
+	spotlight_current_range += 1
+	refresh_light()
+	
+func refresh_light():
+	spotlight.spot_range = spotlight_ranges[spotlight_current_range % len(spotlight_ranges)]
