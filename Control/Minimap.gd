c35ab55710d1d9e0dc9f8ac1277be923efcd76a2
gilzoide <gilzoide@gmail.com>
2020-05-11 21:48:39 -0300

    Simplify MinimapDetector, add compass north

--- Control/Minimap.gd
+++ Control/Minimap.gd
@@ -1,9 +1,15 @@
 extends Control
 
+export(Texture) var compass_point_tex
+onready var compass_point_size = compass_point_tex.get_size()
+onready var compass_point_margin = compass_point_size * 0.75
+onready var compass_point_half_size = compass_point_size * 0.5
 export(Color) var background_color = Color(0.25, 0.25, 0.25)
 export(Color) var glow_color = Color(1.0, 1.0, 1.0)
 export(float) var glow_size = 4
+
 var points: PoolVector2Array
+var rotation: float
 
 func _draw():
 	var side = min(rect_size.x, rect_size.y)
@@ -14,7 +20,13 @@ func _draw():
 	var inset_radius = (side - glow_size) * 0.5
 	for p in points:
 		draw_circle(center + (p.clamped(1.0) * inset_radius), glow_size, glow_color)
+	var compass_point = Vector2(0, -1.1).rotated(rotation) * (inset_radius + compass_point_margin.y)
+	draw_texture(compass_point_tex, center + compass_point - compass_point_half_size)
 
-func set_points(points: PoolVector2Array):
-	self.points = points
+func set_points(detected_points: PoolVector2Array):
+	points = detected_points
+	update()
+
+func set_rotation(detector_rotation: float):
+	rotation = detector_rotation
 	update()
